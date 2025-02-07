63680658465ad0546fa3d9c199d6fc7bda7884c5
miroshnik <miroshnik@sportxline.com>
2025-02-07 11:41:15 +0100

    MVP

--- src/index.ts
+++ src/index.ts
@@ -1,83 +1,137 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.json`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import puppeteer from '@cloudflare/puppeteer';
+import { GoogleGenerativeAI } from '@google/generative-ai';
 
 interface Env {
 	KV: KVNamespace;
-	// ... other binding types
+	PROCESSED_POSTS_KV: KVNamespace;
+
+	BROWSER: Fetcher;
+
+	GOOGLE_GEMINI_API_KEY: string;
+	TELEGRAM_CHAT_ID: string;
+	TELEGRAM_BOT_TOKEN: string;
 }
 
-interface RedditPost {
-	id: string;
-	title: string;
-	videoUrl: string | null;
-}
+const prompt = `мне нужен ответ в виде хештегов через пробел для строки о событии в матче, где будут команды и игроки или игрок, если он один;
+пример: для строки "Metz 1-0 Grenoble - Pape Amadou Diallo 42'" ответ будет: #Metz #Grenoble #PapeAmadouDiallo;
+пример: для строки "Léo Pelé (Athletico-PR) faces racist insults after being sent off against Coritiba" ответ будет: #AthleticoPR #Coritiba #LéoPelé;
+так же нужны теги для самого события, типа ВАР, удаление, пенальти, красная карта, гол, травма и т.д.; OG - own goal;
+пример: Milan 2-2 Parma - Strahinja Pavlović VAR Disallow -> #Milan #Parma #StrahinjaPavlović #VAR #GoalDiasallowed;
+в хештегах должны быть только буквы;
+если есть только фамилия игрока или только клуб, то нужны только соответствующие хештеги;
+сначала команды, потом игроки;
+ответ только на английском;
+строка: `;
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return Response.json(await env.KV.get('processedPosts', 'json'));
+	async fetch (request, env): Promise<Response> {
+		return Response.json(await env.KV.list());
 	},
 
-	async scheduled (event, env, ctx) {
-		console.log(`Scheduled event ${event}`);
-
-		let processedPosts: Record<string, number> = await env.KV.get('processedPosts', 'json') ?? {}; // id => timestamp
-
+	async scheduled (event, env) {
 		try {
+			const genAI = new GoogleGenerativeAI(env.GOOGLE_GEMINI_API_KEY);
+			const model = genAI.getGenerativeModel({ model: 'gemini-2.0-flash-exp' });
+
 			const response = await fetch('https://www.reddit.com/r/soccer/new.json?limit=25', {
 				headers: { 'User-Agent': 'Mozilla/5.0' }
 			});
 
 			if (!response.ok) {
-				console.error(`Ошибка HTTP: ${response.status}`);
+				console.error(`Posts fetch error: ${response.status} ${response.statusText}`);
 
 				return;
 			}
 
 			const data: Record<any, any> = await response.json();
 
-			const mediaPosts: RedditPost[] = data.data.children
-				.filter((post: any) => {
-					return post.data.link_flair_text === 'Media' && !processedPosts[post.data.id];
-				})
-				.map((post: any) => {
-					const title = post.data.title;
-					const videoUrl = post.data.media?.reddit_video?.fallback_url || post.data.url;
-					const id = post.data.id;
+			for (const item of data.data.children.filter((post: any) => post.data.link_flair_text === 'Media').reverse()) {
+				const id = item.data.id;
+				const title = item.data.title;
+				let videoUrl = item.data.media?.reddit_video?.fallback_url || item.data.url;
 
-					return { id, title, videoUrl };
-				});
+				if (await env.KV.get(id)) {
+					continue;
+				}
 
-			for (const post of mediaPosts.reverse()) {
-				console.log(`ID: ${post.id}`);
-				console.log(`Title: ${post.title}`);
+				console.log(`ID: ${id}, Title: ${title}. Processing.`);
 
-				processedPosts[post.id] = Date.now();
-			}
+				if (videoUrl && !(
+					videoUrl.includes('youtube') || videoUrl.includes('youtu.be') || videoUrl.includes('.jpeg') || videoUrl.includes('.png')
+					|| ((videoUrl.includes('.m3u8') || videoUrl.includes('.mp4')) && !videoUrl.includes('DASH_96.')))
+				) {
+					console.log(`Open ${videoUrl} in browser...`);
+					videoUrl = await getFinalStreamUrl(env, videoUrl);
+				}
 
-			// Cleanup expired IDs
-			const currentTime = Date.now()
-			const expirationTime = 24 * 60 * 60 * 1000 // Храним ID сутки
+				console.log(`Video URL: ${videoUrl}, ID: ${id}`);
 
-			for (const [id, timestamp] of Object.entries(processedPosts)) {
-				if (currentTime - timestamp > expirationTime) {
-					console.log('Delete expired post', { id, timestamp })
-					delete processedPosts[id]
+				if (videoUrl) {
+					let message = `${title} <a href="${videoUrl}">↗</a>`;
+					if ((videoUrl.includes('.mp4') || videoUrl.includes('.m3u8')) && !videoUrl.includes('DASH_96.')) {
+						message += ` <a href="https://demo.meshkov.info/video?url=${encodeURIComponent(videoUrl)}">▷</a>`;
+					}
+					message += `\n${(await model.generateContent(prompt + title)).response.text()}`;
+
+					const response = await fetch(`https://api.telegram.org/bot${env.TELEGRAM_BOT_TOKEN}/sendMessage`, {
+						method: 'POST',
+						headers: {
+							'Content-Type': 'application/json'
+						},
+						body: JSON.stringify({
+							chat_id: env.TELEGRAM_CHAT_ID,
+							text: message,
+							parse_mode: 'HTML'
+						})
+					});
+
+					if (!response.ok) {
+						console.error(`Failed to send Telegram message: ${response.status}`);
+						continue;
+					}
+
+					await env.KV.put(id, videoUrl, { expirationTtl: 24 * 60 * 60 }); // store for 1 day (in seconds)
+					await env.PROCESSED_POSTS_KV.put(id, videoUrl, { expirationTtl: 24 * 60 * 60 }); // store for 1 day (in seconds)
 				}
 			}
-
-			await env.KV.put('processedPosts', JSON.stringify(processedPosts));
 		} catch (error) {
 			console.error(error);
 		}
 	}
 } satisfies ExportedHandler<Env>;
+
+const getFinalStreamUrl = async (env: Env, streamUrl: string): Promise<string | null> => {
+	const browser = await puppeteer.launch(env.BROWSER);
+
+	const page = await browser.newPage();
+
+	await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36');
+
+	let finalStreamUrl: string | null = null;
+	try {
+		await page.setRequestInterception(true);
+
+		page.on('request', async (request) => {
+			const url = request.url();
+
+			if (!finalStreamUrl && ((url.includes('.m3u8') || url.includes('.mp4')) && !url.includes('DASH_96.'))) {
+				console.log(`Video stream found: ${url}`);
+				finalStreamUrl = url;
+			} else {
+				await request.continue();
+			}
+		});
+
+		await page.goto(streamUrl, { waitUntil: 'domcontentloaded' });
+
+		await page.waitForSelector('video source', { timeout: 3000 }); // Waiting for video.stream element
+
+		return finalStreamUrl;
+	} catch (error) {
+		console.error(`Stream processing fail for ${streamUrl}:`, error);
+
+		return finalStreamUrl;
+	} finally {
+		await browser.close();
+	}
+};
